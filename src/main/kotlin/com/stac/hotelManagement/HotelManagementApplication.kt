5df86fd101850090029fc574694100126d0cac74
takamandre <andre@pluriel.tech>
2025-06-23 19:01:20 +0100

    HTR-2: hotel management init commit

---
+++ src/main/kotlin/com/stac/hotelManagement/HotelManagementApplication.kt
@@ -0,0 +1,11 @@
+package com.stac.hotelManagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HotelManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<HotelManagementApplication>(*args)
+}
