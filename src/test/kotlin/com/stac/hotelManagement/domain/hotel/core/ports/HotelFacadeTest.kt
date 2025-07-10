1652edaf341525d9abc5b8c0823d702cefcdf2b3
takamandre <andre@pluriel.tech>
2025-07-10 21:11:05 +0100

    HTR-2: added amenity to hotel test cases

--- src/test/kotlin/com/stac/hotelManagement/domain/hotel/core/ports/HotelFacadeTest.kt
+++ src/test/kotlin/com/stac/hotelManagement/domain/hotel/core/ports/HotelFacadeTest.kt
@@ -1,11 +1,17 @@
 package com.stac.hotelManagement.domain.hotel.core.ports
 
 import com.stac.hotelManagement.domain.hotel.core.ports.outgoing.HotelDatabase
+import com.stac.hotelManagement.infrastruture.common.models.enums.EntityType
+import com.stac.hotelManagement.infrastruture.common.services.checkEntityExistence.AmenityChecker
+import com.stac.hotelManagement.infrastruture.common.services.checkEntityExistence.EntityExistenceCheckerFactory
 import com.stac.hotelManagement.util.FakerUtils.fakeAddHotelCommand
+import com.stac.hotelManagement.util.FakerUtils.fakeAmenity
 import com.stac.hotelManagement.util.FakerUtils.fakeHotel
 import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.Test
+import org.springframework.http.HttpStatus
+import org.springframework.web.server.ResponseStatusException
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
@@ -14,7 +20,10 @@ import java.util.UUID
 class HotelFacadeTest {
 
   private val database = mockk<HotelDatabase>()
-  private val facade = HotelFacade(database)
+  private val existenceCheckerFactory = mockk<EntityExistenceCheckerFactory>()
+  private val facade = HotelFacade(database, existenceCheckerFactory)
+
+  private val amenityChecker = mockk<AmenityChecker>()
 
   @Test
   fun `should create a new hotel`() {
@@ -40,5 +49,36 @@ class HotelFacadeTest {
     StepVerifier.create(facade.getHotels())
       .expectNextSequence(hotelDtos)
       .verifyComplete()
+  }
+
+  @Test
+  fun`add amenity completes successfully`(){
+    val amenityId = UUID.randomUUID()
+    val hotelId = UUID.randomUUID()
+    val findHotel = fakeHotel().copy(id = hotelId, amenities = mutableListOf())
+    val addedAmenity = fakeAmenity().copy(id = amenityId)
+    val updatedHotel = findHotel.addAmenity(addedAmenity.id!!)
+
+    every { existenceCheckerFactory.checker(EntityType.AMENITY.name) } returns amenityChecker
+    every { amenityChecker.exists(amenityId) } returns Mono.just(true)
+    every { database.findById(hotelId) } returns Mono.just(findHotel)
+    every { database.save(any()) } returns Mono.just(updatedHotel)
+
+    StepVerifier.create(facade.addAmenity(hotelId, amenityId))
+      .expectNextMatches { it.amenities?.size == 1 }
+      .verifyComplete()
+  }
+
+  @Test
+  fun`add amenity completes with error if amenity doesn't exists`(){
+    val amenityId = UUID.randomUUID()
+    val hotelId = UUID.randomUUID()
+
+    every { existenceCheckerFactory.checker(EntityType.AMENITY.name) } returns amenityChecker
+    every { amenityChecker.exists(amenityId) } returns Mono.just(false)
+
+    StepVerifier.create(facade.addAmenity(hotelId, amenityId))
+      .expectErrorMatches { it is ResponseStatusException && it.statusCode == HttpStatus.NOT_FOUND }
+      .verify()
   }
 }
